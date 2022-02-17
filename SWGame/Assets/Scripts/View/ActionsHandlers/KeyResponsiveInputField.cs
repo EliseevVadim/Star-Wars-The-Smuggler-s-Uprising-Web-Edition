c99b3a765cf25a9534e26e152a22679ca94444db
unknown <eliseevv02@mail.ru>
2022-02-17 10:19:17 +0300

    feat: add possibility to send messages via pressing the left enter button

---
+++ SWGame/Assets/Scripts/View/ActionsHandlers/KeyResponsiveInputField.cs
@@ -0,0 +1,25 @@
+﻿using SWGame.Activities;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SWGame.View.ActionsHandlers
+{
+    public class KeyResponsiveInputField : MonoBehaviour
+    {
+        [SerializeField] InputField _responsiveField;
+
+        void Start()
+        {
+            _responsiveField.onEndEdit.AddListener(delegate { SendMessageViaPressingEnterButton(); });
+        }
+
+        private void SendMessageViaPressingEnterButton()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {         
+                GetComponentInParent<ChatMessageSender>().Send();
+            }
+        }
+    }
+}
